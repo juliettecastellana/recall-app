3467ed1db8b4e806a7a5b034efc665d6a2c153ef
Juliette Castellana <juliettexhs77@gmail.com>
2026-05-26 15:53:33 -0400

    Fix next config

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,6 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
-
-const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
